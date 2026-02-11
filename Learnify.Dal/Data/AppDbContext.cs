b1577a8781ee2af078f5e6214e420ed24256d197
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-02-11 14:57:25 +0500

    fc

--- Learnify.Dal/Data/AppDbContext.cs
+++ Learnify.Dal/Data/AppDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Learnify.DAL.Data;
+﻿using Learnify.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learnify.DAL.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+
 }
