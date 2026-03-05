010bac51c5f47e3b7b45b6072dac2057b6759769
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-03-05 14:09:03 +0500

    f

---
+++ Learnify/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learnify.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Courses()
+        {
+            return View();
+        }
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
