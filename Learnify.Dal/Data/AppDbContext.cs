88df231740f8e406eae9bc6b2b0a36ba1a118631
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-03-31 14:09:59 +0500

    rg

--- Learnify.Dal/Data/AppDbContext.cs
+++ Learnify.Dal/Data/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace Learnify.DAL.Data;
 
 public class AppDbContext : DbContext
 {  
-    public AppDbContext(DbContextOptions<AppDbContext> options)
+    public AppDbContext(DbContextOptions<AppDbContext> options)  
         : base(options)
     {
     } 
